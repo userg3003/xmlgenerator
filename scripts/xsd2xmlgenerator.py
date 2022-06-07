46babdb830773ffc2fd8a8687afc3196234cc93b
userg61 <s.urvanov@gmail.com>
2022-06-07 16:39:11 +0300

    Добавить обработку типов XsdUnion.

--- scripts/xsd2xmlgenerator.py
+++ scripts/xsd2xmlgenerator.py
@@ -5,13 +5,14 @@ from faker import Faker
 from faker.providers import date_time, internet, lorem, person, python
 
 import xmlschema
+from xmlschema.validators import XsdUnion
 import rstr
 
 from scripts.utils.fakers import Fakers
 
 random.seed()
 
-Faker.seed(random.randint(1, 100))
+Faker.seed(random.randint(1, 1000))
 
 
 def make_fake():
@@ -56,7 +57,7 @@ class Xsd2XmlGenerator:
             xml_node.text = self.get_value_for_attribute(xsd_node, xsd_node.type, fake_value)
         # complex types
         else:
-            logger.debug(f"{xsd_node.type}  {xsd_node.type.content}")
+            # logger.debug(f"{xsd_node.type}  {xsd_node.type.content}")
             group = getattr(xsd_node.type.content, "_group", [])
             for sub_node in group:
                 if sub_node.occurs[1] is None:
@@ -141,24 +142,91 @@ class Xsd2XmlGenerator:
             elif node.name == "МесГодРожд":
                 value = self.cur_birthday[-7:]
         if value is None:
-            local_type_name = node.type.local_name
+            # logger.debug(f"{node.type}")
             if node.type.enumeration is not None:
                 value = self.fake.random_element(elements=node.type.enumeration)
                 return value
-            if local_type_name is None:
-                local_type_name = node.type.base_type.local_name
-            if local_type_name == "string":
-                length = node.type.max_length - 1 - len(node.name)
-                if length < 0:
-                    length = node.type.max_length
-                if length >= 10:
-                    length = random.randint(10, length)
-                    text = self.fake.text(max_nb_chars=length)
-                else:
-                    text = self.fake.word()[:length]
-                value = f"{node.name} {text}"[:length]
-            elif local_type_name == "boolean":
+            all_types = self.parse_type(node.type)
+            if isinstance(all_types, list):
+                value = self.generate_value(all_types, node.name)
+                return value
+
+            type_name = all_types
+            if type_name == "boolean":
                 value = random.choice(["true", "false"])
             else:
                 value = "????"
+        return value
+
+    def generate_value(self, types, node_name):
+        value = ""
+        index = random.randint(0, len(types) - 1)
+
+        if types[index]['type_name'] == "string":
+            if types[index]['max_length'] == 0:
+                return ""
+            length = types[index]['max_length'] - 1 - len(node_name)
+            if length < 0:
+                length = types[index]['max_length']
+            if length >= 10:
+                length = random.randint(10, length)
+                text = self.fake.text(max_nb_chars=length)
+            else:
+                text = self.fake.word()[:length]
+            value = f"{node_name} {text}"[:length]
+        elif types[index]['type_name'] == "decimal":
+            fractionDigits = 0
+            if types[index]['fractionDigits'] is not None:
+                all_digits = types[index]['totalDigits'] - types[index]['fractionDigits']
+                fractionDigits = str(
+                    self.fake.random_number(digits=types[index]['fractionDigits'], fix_len=False))
+            else:
+                all_digits = types[index]['totalDigits']
+            value = str(self.fake.random_number(digits=all_digits, fix_len=True))
+            value = value[0:random.randint(1, all_digits)]
+            if types[index]['fractionDigits'] is not None:
+                value = f"{value}.{fractionDigits}"
+        elif types[index]['type_name'] == "boolean":
+            logger.debug(f"boolean types[index] {types[index]}")
+            value = random.choice(["true", "false"])
+
+        return value
+
+    @staticmethod
+    def parse_type(node_type):
+        if isinstance(node_type, XsdUnion):
+            node_types = list()
+            for member_type in node_type.member_types:
+                type_ = {
+                    "type_name": member_type.root_type.local_name,
+                    "max_length": member_type.max_length,
+                    "min_length": member_type.min_length,
+                    "max_value": member_type.max_value,
+                    "min_value": member_type.min_value,
+                    "totalDigits": Xsd2XmlGenerator.getValueFromFacet(member_type.facets, "totalDigits"),
+                    "fractionDigits": Xsd2XmlGenerator.getValueFromFacet(member_type.facets, "fractionDigits"),
+                    "length": Xsd2XmlGenerator.getValueFromFacet(member_type.facets, "length"),
+                    "maxLength": Xsd2XmlGenerator.getValueFromFacet(member_type.facets, "maxLength"),
+                    "patterns": member_type.patterns
+                }
+                if member_type.patterns is not None:
+                    logger.debug(f"patterns: {member_type.patterns}")
+                node_types.append(type_)
+            return node_types
+
+        local_type_name = getattr(node_type, "local_name")
+        if local_type_name is not None:
+            logger.debug(f"local_type_name from node.type: {local_type_name}")
+            return local_type_name
+        local_type_name = getattr(node_type.base_type, "local_name")
+        if local_type_name is None:
+            logger.debug(f"local_type_name from node.base_type: {local_type_name}")
+            return local_type_name
+        logger.debug(f"NodeType not defined")
+        return None
+
+    @staticmethod
+    def getValueFromFacet(facet, nameValue):
+        nameKey = f'{{http://www.w3.org/2001/XMLSchema}}{nameValue}'
+        value = facet[nameKey].value if facet.get(nameKey, None) else None
         return value
